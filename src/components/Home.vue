69134b304d2fc4363066ecab9b302b81ca3ac422
begimov <begimov@gmail.com>
2018-07-19 11:19:41 +0300

    styles fixed

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1072,27 +1072,11 @@
 
 <script src="./home.js"></script>
 
-<style scoped>
+<style>
 h1,
 h2,
 h3,
 .text-accent {
   font-family: "Noto Serif", serif;
-}
-.countdown {
-  font-size: 1.8em;
-  padding: 10px;
-  color: #fff;
-  box-shadow: 0px 2px rgb(255, 248, 183);
-  background: rgb(90, 36, 0);
-  /* For browsers that do not support gradients */
-  background: -webkit-linear-gradient(rgb(196, 95, 0), #ffae00);
-  /* For Safari 5.1 to 6.0 */
-  background: -o-linear-gradient(rgb(196, 95, 0), #ffae00);
-  /* For Opera 11.1 to 12.0 */
-  background: -moz-linear-gradient(rgb(196, 95, 0), #ffae00);
-  /* For Firefox 3.6 to 15 */
-  background: linear-gradient(rgb(196, 95, 0), #ffae00);
-  /* Standard syntax */
 }
 </style>
